5053d9d5c2509d053c9633bb1f83ab0010c441e3
Joshua Cold <joshua.cold@microfocus.com>
2020-10-15 12:39:14 -0600

    inital commit

---
+++ k8s-dock-profile.py
@@ -0,0 +1,148 @@
+import sys
+from argparse import ArgumentParser
+from time import sleep
+import subprocess
+import signal
+import re
+import os
+stat_map = {}
+con_map = {}
+
+
+def signal_handler(sig, frame):
+    if args.file:
+        print('\n writing to file ==> ', args.file)
+    else:
+        file = f"k8s-dock.profile"
+        print('\n writing to file ==> ', file)
+        f = open(file, "w")
+        content = ""
+        if args.docker:
+            for key, value in con_map.items():
+                print(f"{key} ==> {value}")
+                content += f"docker container: {key} {0: <16} max cpu: {value['mhz']}mhz max ram {value['mb']}mb image: {value['image']}  id: {value['id']}\n"
+        else:
+            for key, value in stat_map.items():
+                print(f"{key} ==> {value}")
+                content += f"k8s pod name: {key} {0: <16} cpu: {value['cpu']}mhz mem: {value['mem']}Mi \n"
+        content.format()
+        f.write(content)
+        f.close()
+    sys.exit(0)
+
+
+def parse_int(string):
+    return int(re.sub(r'[^\d-]+', '', string))
+
+
+def profile_docker():
+    print("Profiling docker containers on local machine")
+    list_command = 'docker container ls --no-trunc --format "{{.ID}} {{.Names}} {{.Image}}"'
+    normal = subprocess.Popen(list_command, stdout=subprocess.PIPE, shell=True)
+    containers = normal.communicate()[0].decode("utf-8")
+    containers = [line for line in containers.split('\n') if line.strip() != '']
+    print("Running off these containers => ", containers)
+    if not containers:
+        raise Exception(f"found no containers with {list_command}")
+    for con in containers:
+        if con:
+            a = con.split()
+            con_map[a[1]] = {
+                "id": a[0],
+                "image": a[2],
+                "last_read_cpu": [],
+                "mhz": 0.0,
+                "mb": 0.0
+            }
+    while True:
+        for con_name, con_val in con_map.items():
+            try:
+                cpu_file = open(f"/sys/fs/cgroup/cpu,cpuacct/docker/{con_val['id']}/cpuacct.usage_percpu")
+                cpu_stats = cpu_file.read()
+                cpu_file.close()
+
+                ram_file = open(f"/sys/fs/cgroup/memory/docker/{con_val['id']}/memory.max_usage_in_bytes")
+                ram_stats = float(ram_file.read())
+                ram_file.close()
+                # Megabytes = Bytes ÷ 1,048,576
+                ram_mb = ram_stats/1048576
+                # RAM Operation
+                if ram_mb > float(con_val["mb"]):
+                    print(f"{con_name} RAM ==> {ram_mb}mb")
+                    con_map[con_name]["mb"] = ram_mb
+
+                # CPU Operation
+                core_stats = cpu_stats.split()
+                if not con_val["last_read_cpu"]:
+                    con_val["last_read_cpu"] = core_stats
+                    continue
+                else:
+                    # subtract last read value from new value
+                    result = 0
+                    for i in range(len(core_stats)):
+                        result += (int(core_stats[i]) - int(con_val["last_read_cpu"][i]))
+                    result = result / 1000000  # hz => mhz = hz/1 mil
+                    if result > float(con_map[con_name]["mhz"]):
+                        print(f"{con_name} CPU ==> {result}mhz")
+                        con_map[con_name]["mhz"] = result
+                    con_val["last_read_cpu"] = core_stats
+            except FileNotFoundError:
+                # Replace a container in con_map based on container name if its restarted,killed
+                # and its container id has changed.
+                print("cgroup file not found, container restart or killed. Attempting to replace and continue ==> ", con_name)
+                list_command = 'docker container ls --no-trunc --format "{{.ID}} {{.Names}} {{.Image}}"'
+                normal = subprocess.Popen(list_command, stdout=subprocess.PIPE, shell=True)
+                containers = normal.communicate()[0].decode("utf-8").split("\n")
+                for con in containers:
+                    if con:
+                        a = con.split()
+                        if con_name is a[1]:
+                            con_map[a[1]]["id"] = a[0]
+
+
+def profile_k8s():
+    if args.namespace:
+        namespace_arg = f"-n ${args.namespace}"
+        print(f"Profiling kubernetes pods in {args.namespace} namespace")
+        command = f"kubectl top pods {namespace_arg} | sed 1d "
+    else:
+        print("Profiling kubernetes pods in default namespace")
+        command = "kubectl top pods -n default | sed 1d "
+
+    while True:
+        normal = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        stats = normal.communicate()[0].decode("utf-8")
+        print(stats)
+        pod_list = stats.split("\n")
+        for pod in pod_list:
+            if pod:  # pod is not empty
+                i = pod.split()
+                if i[0] not in stat_map:
+                    stat_map[f"{i[0]}"] = {
+                        "cpu": parse_int(i[1]),
+                        "mem": parse_int(i[2])
+                    }
+                else:
+                    if stat_map[f"{i[0]}"]["cpu"] < parse_int(i[1]):
+                        stat_map[f"{i[0]}"]["cpu"] = parse_int(i[1])
+                    if stat_map[f"{i[0]}"]["mem"] < parse_int(i[2]):
+                        stat_map[f"{i[0]}"]["mem"] = parse_int(i[2])
+        print(stat_map)
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    print("Press Ctrl + C to stop and write results to file")
+    sleep(2)
+    parser = ArgumentParser()
+    parser.add_argument("-d", "--docker", help="profile docker containers", action="store_true")
+    parser.add_argument("-k", "--kubernetes", help="profile k8s pods containers", action="store_true")
+    parser.add_argument("-n", "--namespace", help="choose k8s namespace")
+    parser.add_argument("-f", "--file", help="choose file to output to")
+    args = parser.parse_args()
+    if args.docker:
+        profile_docker()
+    if args.kubernetes:
+        if not os.environ.get("KUBECONFIG") and not os.path.isfile("~/.kube/config"):
+            raise Exception("KUBECONFIG env var and ~/.kube/config not set")
+        profile_k8s()
---
+++ readme.md
@@ -0,0 +1,81 @@
+# Objective
+Sometimes when deploying k8s pods, resource management is of more importance
+
+In pod definitions you can create `resource requests` to determine how much an
+individual container should be allowed to use within the pod. 
+
+```yaml
+resources:
+  requests:
+    cpu: 500m
+    memory: 500Mi
+```
+
+Result:
+cpu => throttled to mhz / % of a single core on k8s node
+ram => if exceeding limit then restart container
+
+If you are deploying an application you yourself did not create then it can be tricky 
+to determine how much cpu and ram to allocate
+
+This script will give you an accurate stats on either docker containers or k8s pods 
+over the course of their workload so you can make better decisions on your resource requests
+
+This script output the max utilized CPU and RAM in mb/mhz
+
+If you are deploying for one of these scenarios then this script can be helpful:
+
+- On Prem K8S cluster with limited resources
+- Deploying many testing pods 
+- unsure how much resources to allocate an individual container
+- get stats on an application during any workload
+
+> Personally in my setup I deploy many testing applications in parallel. If I over utilize the cluster then k8s will have pods wait their turn for resources. This is essential because my application consumes about 6CPU 10 GB RAM per each pod.
+
+
+Some added bonuses to resource requests in k8s pods:
+
+- k8s only schedules pods if requested resources are available 
+- k8s pods don't get a chance to kill Nodes on the cluster
+
+# Usage
+
+```sh
+usage: k8s-dock-profile.py [-h] [-d] [-k] [-n NAMESPACE] [-f FILE]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d, --docker          profile docker containers
+  -k, --kubernetes      profile k8s pods containers
+  -n NAMESPACE, --namespace NAMESPACE
+                        choose k8s namespace
+  -f FILE, --file FILE  choose file to output to
+```
+
+To start profiling on k8s pods. Note: this just gathers from `kubectl top` 
+
+```sh
+python3 ./k8s-dock-profile.py -k
+```
+
+Profile on k8s pods in namespace
+
+```sh
+python3 ./k8s-dock-profile.py -k -n my-namespace
+```
+
+Docker containers on local machine
+```sh
+python3 ./k8s-dock-profile.py -d
+```
+
+By default results output to `k8s-dock.profile`
+
+To output to a different file
+
+```sh
+python3 ./k8s-dock-profile.py -d -f my-file.txt
+```
+
+To Stop the Application send a SigTERM `Ctrl + C`
+
