c3d046e9e650c5fb7325ecfbbdb28601ee7aafbb
Joshua Cold <joshua.cold@microfocus.com>
2021-04-28 11:31:35 -0600

    add back in docker profiling

--- k8s-dock-profile.py
+++ k8s-dock-profile.py
@@ -13,12 +13,16 @@ con_dict = {}
 
 def signal_handler(sig, frame):
     try:
-        for pod in con_dict:
-            con_dict[pod]['cpu'] = 0
-            con_dict[pod]['mem'] = 0
-            for container in con_dict[pod]['containers']:
-                con_dict[pod]['cpu'] += con_dict[pod]['containers'][container]['cpu']
-                con_dict[pod]['mem'] += con_dict[pod]['containers'][container]['mem']
+        if not args.docker:
+            for pod in con_dict:
+                con_dict[pod]['cpu'] = 0
+                con_dict[pod]['mem'] = 0
+                for container in con_dict[pod]['containers']:
+                    con_dict[pod]['cpu'] += con_dict[pod]['containers'][container]['cpu']
+                    con_dict[pod]['mem'] += con_dict[pod]['containers'][container]['mem']
+        else:
+            for key, value in con_dict.items():
+                value.pop('last_read_cpu', None)
         file = "kd-profile.json"
         if args.file:
             file = args.file
@@ -35,6 +39,86 @@ def signal_handler(sig, frame):
 
 def parse_int(string):
     return int(re.sub(r'[^\d-]+', '', string))
+
+def profile_docker():
+    print("Profiling docker containers on local machine")
+    list_command = 'docker container ls --no-trunc --format "{{.ID}} {{.Names}} {{.Image}}"'
+    normal = subprocess.Popen(list_command, stdout=subprocess.PIPE, shell=True)
+    containers = normal.communicate()[0].decode("utf-8")
+    containers = [line for line in containers.split('\n') if line.strip() != '']
+    print("Running off these containers => ", containers)
+    if not containers:
+        raise Exception(f"found no containers with {list_command}")
+    for con in containers:
+        if con:
+            a = con.split()
+            con_dict[a[1]] = {
+                "id": a[0],
+                "image": a[2],
+                "last_read_cpu": [],
+                "mhz": 0.0,
+                "mb": 0.0
+            }
+
+    while True:
+        for con_name, con_val in con_dict.items():
+            try:
+                paths = [line[0:].decode("utf-8") for line in subprocess.check_output(f"find /sys/fs/cgroup -iname {con_val['id']} || true", shell=True).splitlines()]
+                paths_dct = {paths[i].split("/")[4]: paths[i] for i in range(0, len(paths))}
+
+                cpu_file = open(paths_dct["cpu,cpuacct"] + "/cpuacct.usage_percpu")
+                cpu_stats = cpu_file.read()
+                cpu_file.close()
+
+                ram_file = open(paths_dct["memory"] + "/memory.max_usage_in_bytes")
+                ram_stats = float(ram_file.read())
+                ram_file.close()
+                # Megabytes = Bytes ÷ 1,048,576
+                ram_mb = ram_stats/1048576
+                # RAM Operation
+                if ram_mb > float(con_val["mb"]):
+                    print(f"{con_name} RAM ==> {ram_mb}mb")
+                    con_dict[con_name]["mb"] = ram_mb
+
+                # CPU Operation
+                core_stats = cpu_stats.split()
+                if not con_val["last_read_cpu"]:
+                    con_val["last_read_cpu"] = core_stats
+                    continue
+                else:
+                    # subtract last read value from new value
+                    result = 0
+                    for i in range(len(core_stats)):
+                        result += (int(core_stats[i]) - int(con_val["last_read_cpu"][i]))
+                    result = result / 1000000  # hz => mhz = hz/1 mil
+                    if result > float(con_dict[con_name]["mhz"]):
+                        print(f"{con_name} CPU ==> {result}mhz")
+                        con_dict[con_name]["mhz"] = result
+                    con_val["last_read_cpu"] = core_stats
+            except FileNotFoundError:
+                # Replace a container in con_dict based on container name if its restarted,killed
+                # and its container id has changed.
+                print("cgroup file not found, container restart or killed. Attempting to replace and continue ==> ", con_name)
+                list_command = 'docker container ls --no-trunc --format "{{.ID}} {{.Names}} {{.Image}}"'
+                normal = subprocess.Popen(list_command, stdout=subprocess.PIPE, shell=True)
+                containers = normal.communicate()[0].decode("utf-8").split("\n")
+                for con in containers:
+                    if con:
+                        a = con.split()
+                        if con_name is a[1]:
+                            con_dict[a[1]]["id"] = a[0]
+            except KeyError:
+                print(f"Key Error Attempting to replace: Container Name:{con_name} Container Data:{con_val} Container Paths:{paths_dct}")
+                list_command = 'docker container ls --no-trunc --format "{{.ID}} {{.Names}} {{.Image}}"'
+                normal = subprocess.Popen(list_command, stdout=subprocess.PIPE, shell=True)
+                containers = normal.communicate()[0].decode("utf-8").split("\n")
+                for con in containers:
+                    if con:
+                        a = con.split()
+                        if con_name is a[1]:
+                            con_dict[a[1]]["id"] = a[0]
+            except Exception as err:
+                print(f"Exception! something went wrong but im continuing {err}")
 
 def profile_k8s():
     if args.label_selectors:
@@ -77,6 +161,7 @@ def profile_k8s():
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-n", "--namespace", help="choose k8s namespace")
+    parser.add_argument("-d", "--docker", help="just profile docker containers", action="store_true")
     parser.add_argument("-f", "--file", help="choose file to output to")
     parser.add_argument("-s", "--search", help="fuzzy match search term using grep")
     parser.add_argument("-l", "--label-selectors", help="k8s selector query ex: key1=value1,key2=value2")
@@ -90,10 +175,17 @@ if __name__ == "__main__":
     if not environ.get("KUBECONFIG") and not path.isfile("~/.kube/config"):
         raise Exception("KUBECONFIG env var and ~/.kube/config not set")
     if args.timeout:
-        p = threading.Thread(target=profile_k8s, args=())
+        if args.docker:
+            p = threading.Thread(target=profile_docker, args=())
+        else:
+            p = threading.Thread(target=profile_k8s, args=())
         p.daemon = True
         p.start()
         sleep(int(args.timeout))
+        print("Hit timeout finishing up... ")
         signal_handler("","")
     else:
-        profile_k8s()
+        if args.docker:
+            profile_docker()
+        else:
+            profile_k8s()
